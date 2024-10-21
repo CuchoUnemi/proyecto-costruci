d7fb16b693eb2c74bec4fec82948dc9c39808cde
cquevedom2 <cquevedom2@unemi.edu.ec>
2024-10-21 11:19:14 -0500

    Corrección de Name en app.py de WebScraping

--- core/webscraping/apps.py
+++ core/webscraping/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WebscrapingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webscraping'
+    name = 'core.webscraping'
